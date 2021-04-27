5c587da066127124bdb711221311270865f4ad0b
Monica Bui <monicabbui@gmail.com>
2021-04-26 17:09:57 -0700

    Create repo, build skeleton, start on Two Number Sum

---
+++ Two Number Sum/TwoNumberSum.js
@@ -0,0 +1,15 @@
+
+
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
+
+// Test
+const array1 = [3, 5, -4, 8, 11, 1, -1, 6];
+const targetSum1 = 10;
+const test1 = twoNumberSum(array1, targetSum1);
+
+console.log(test1);
